44acfc376b947cfc0eec6c02f3a9cf6df9eae2b9
Martin <martinlsalinas@pm.me>
2025-03-06 10:00:15 -0600

    add all contacts

---
+++ app/components/AllContacts.js
@@ -0,0 +1,17 @@
+export default function AllContacts() { 
+
+  const exampleInfo = [ 
+    {name: 'Martin', Email: 'martin@gmail.com', phone: 111222333, imageUrl: 'www.picture.com', id:1}, 
+    {name: 'Martin', Email: 'martin@gmail.com', phone: 111222333, imageUrl: 'www.picture.com', id:2}, 
+    {name: 'Martin', Email: 'martin@gmail.com', phone: 111222333, imageUrl: 'www.picture.com', id: 3}, 
+    {name: 'Martin', Email: 'martin@gmail.com', phone: 111222333, imageUrl: 'www.picture.com', id: 4}
+  ]
+
+
+
+  return ( 
+    <div> 
+
+    </div>
+  )
+}
