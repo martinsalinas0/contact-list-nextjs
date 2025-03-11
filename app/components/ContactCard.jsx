c193247e5ebaa80c4d551d81285e27656e570459
martinsalinas0 <martinlsalinas@pm.me>
2025-03-10 22:07:13 -0500

    update contactCard

--- app/components/ContactCard.jsx
+++ app/components/ContactCard.jsx
@@ -1,36 +1,54 @@
+
 "use client";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { ContactsAPI } from "../data/contactsAPI";
 
 export default function ContactCard({ contact }) {
   const router = useRouter();
 
   const handleEditClick = () => {
-    router.push(`/contacts/edit/${contact.pid}`);
+    router.push(`/contacts/edit/${contact.id}`);
+  };
+
+  const handleDeleteClick = () => {
+    if (confirm(`Are you sure you want to delete ${contact.name}?`)) {
+      ContactsAPI.deleteContact(contact.id);
+      router.push("/contacts");
+    }
   };
 
   return (
-  
-      <div className="d-flex justify-content-center">
-        <div className="card" style={{ width: "40rem",  width: "50rem" }}>
-          <div className="card-body text-center">
-            <h1 className="card-title">{contact.name} </h1>
-            <img
-              src={contact.imageUrl}
-              className="rounded img-fluid "
-              style={{ width:450, height: 350 }}
-            />
-            <p className="card-text">Phone Number: <strong>{contact.number}</strong></p>
-            <p className="card-text">Email address: <strong>{contact.email}</strong></p>
-            <p>ID#: {contact.pid}</p>
-            <button>Edit</button>
-            <button>Delete</button>
-            <button>Back</button>
-
-
-          </div>
+    <div className="d-flex justify-content-center">
+      <div className="card mt-5" style={{ width: "40rem", height: "40rem" }}>
+        <div className="card-body text-center">
+          <h1 className="card-title">{contact.name} </h1>
+          <img
+            src={contact.imageUrl}
+            className="rounded img-fluid "
+            style={{ width: 450, height: 350 }}
+          />
+          <p className="card-text">
+            Phone Number: <strong>{contact.number}</strong>
+          </p>
+          <p className="card-text">
+            Email address: <strong>{contact.email}</strong>
+          </p>
+          <p>ID#: {contact.id}</p>
+          <button className="btn btn-primary mx-2" onClick={handleEditClick}>
+            Edit
+          </button>
+          <button className="btn btn-danger mx-2" onClick={handleDeleteClick}>
+            Delete
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary mx-2"
+            onClick={() => router.push("/contacts")}
+          >
+            Back
+          </button>
         </div>
       </div>
-   
+    </div>
   );
 }
