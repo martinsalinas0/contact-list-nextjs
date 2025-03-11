8f9025cff6cf52ca2eeecef32ea7fa73c53e72df
martinsalinas0 <martinlsalinas@pm.me>
2025-03-10 22:09:03 -0500

    update contacts/new

--- app/contacts/new/page.jsx
+++ app/contacts/new/page.jsx
@@ -4,7 +4,6 @@ import AddContactForm from "@/app/components/AddContactForm";
 export default function AddContactPage() {
   return (
     <div className="container mt-4">
-      
       <AddContactForm />
     </div>
   );
